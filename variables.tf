e0967811bffcf71ec2d72311879abd7c053e91f9
Patil <patilmaddyg>
2021-03-20 13:18:59 +0530

    variables file

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "subscription_id" {
+    default = ""
+}
+
+variable "client_id" {
+    default = ""
+    
+}
+
+variable "client_secret" {
+    default = ""
+    
+}
+
+variable "tenant_id" {
+    default = ""
+    
+}
